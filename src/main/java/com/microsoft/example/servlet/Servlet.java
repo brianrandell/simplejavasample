08ddc60aa386d48199bf5cb6f2c8a36ccf9c4bd2
Brian A. Randell <brianr@mcwtech.com>
2018-01-15 12:19:25 -0800

    Add sample files

---
+++ src/main/java/com/microsoft/example/servlet/Servlet.java
@@ -0,0 +1,21 @@
+package com.microsoft.example.servlet;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException
+    {
+    	long end = System.currentTimeMillis() + 10000;
+    	while (System.currentTimeMillis() < end){}
+    	
+        PrintWriter pw = response.getWriter();
+        pw.println("hello, world");
+        pw.close();
+    }
+}
